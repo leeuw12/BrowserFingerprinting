731d24ed5740670b762d3c8c03ad1244302beab9
Ivan <i_am_vanya@mail.ru>
2021-05-15 02:10:40 +0300

    pre-project

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,18 +1,23 @@
 import logging
 from logging.handlers import RotatingFileHandler
-from flask import Flask, request, abort
-from pymongo import MongoClient
-import jwt
+
+import flask_login
+from flask import Flask, abort
+from flask import flash, redirect, url_for
 from flask import make_response
-from flask import request, jsonify, Blueprint, render_template
-from flask_login import current_user
+from flask import request, jsonify, render_template
+from flask_login import LoginManager
+from flask_login import current_user, login_required
+from flask_login import logout_user, login_user
+from pymongo import MongoClient
+from werkzeug.security import check_password_hash, generate_password_hash
 
 
 def configure_logging():
     logger = logging.getLogger(__name__)
     logging.getLogger('flask_cors').level = logging.DEBUG
     logger.setLevel(logging.INFO)
-    handler = RotatingFileHandler('info.log', maxBytes=10000, backupCount=1)
+    handler = RotatingFileHandler('info.log', maxBytes=100000000, backupCount=1)
     handler.setLevel(logging.INFO)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
@@ -20,18 +25,58 @@ def configure_logging():
     return logger
 
 
+app = Flask(__name__, static_folder='./static/', template_folder='./templates/')
+app.secret_key = 'c0M`e~t_C1O}d'
+
 client = MongoClient('mongodb', 27017)
 db = client['mydb']
-app = Flask(__name__, static_folder='./static/', template_folder='./templates/')
+users = db.users
+
 logger = configure_logging()
 
+login = LoginManager(app)
+login.login_view = 'login'
 
-# login = LoginManager(app)
+
+@login.user_loader
+def load_user(username):
+    return User.getuser(username, db)
+
+
+class User(flask_login.UserMixin):
+    def __init__(self, user):
+        self.id = str(user['username'])
+        self.user = user
+
+    def get_id(self):
+        username = self.user['username']
+        return str(username)
+
+    @classmethod
+    def getuser(cls, username, db):
+        user = db.users.find_one({"username": username})
+        if user:
+            return User(user)
+        else:
+            return None
 
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('details.html', user=current_user)
+    try:
+        if current_user.is_authenticated():
+            return redirect(url_for("details"))
+        else:
+            redirect(url_for("login"))
+    except:
+        return redirect(url_for("login"))
+
+
+@app.route('/details', methods=['GET'])
+@login_required
+def details():
+    # db.users.insert_one({"name": "Ivan"})
+    return render_template('index.html', user=current_user)
 
 
 @app.route('/foo', methods=['POST'])
@@ -49,3 +94,61 @@ def foo():
     # print(request.json)
     # logger.info(request.json)
     # return json.dumps(request.json)
+
+
+# REGISRTATION
+
+
+@app.route('/login', methods=['GET'])
+def login():
+    try:
+        current_user.is_authenticated()
+        return redirect(url_for("index"))
+    except:
+        return render_template('login.html', error="")
+
+
+@app.route('/register', methods=['GET'])
+def register():
+    try:
+        current_user.is_authenticated()
+        return redirect(url_for("index"))
+    except:
+        return render_template('register.html')
+
+
+@app.route('/login', methods=['POST'])
+def login_post():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    user = db.users.find_one({"username": username})
+    if user:
+        if check_password_hash(str(user['password']), password):
+            login_user(User(user))
+            flash("Successfully logged in")
+            return redirect(url_for('details'))
+    else:
+        return redirect(url_for('login'))
+
+
+@app.route('/register', methods=['POST'])
+def signup_post():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    user = db.users.find_one({"username": username})
+    if user:
+        return redirect(url_for('login'))
+    try:
+        db.users.insert_one({"username": username, "password": generate_password_hash(password, method='sha256')})
+    except Exception as ex:
+        logger.info(ex)
+    return redirect(url_for('login'))
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    user = current_user
+    user.authenticated = False
+    logout_user()
+    return redirect(url_for('index'))
--- flask/app/static/js/sendToServer.js
+++ flask/app/static/js/sendToServer.js
@@ -1,27 +1,99 @@
-var submit_message = function() {
-	fingerprint = document.getElementById('fingerprint').innerHTML;
-	browser = document.getElementById('browser').innerHTML;
-	var entry = {
-		fingerprint: fingerprint,
-		browser: browser
-	};
-	fetch(`${window.origin}/foo`, {
-		method: "POST",
-		credentials: "include",
-		body: JSON.stringify(entry),
-		cache: "no-cache",
-		headers: new Headers({
-			"content-type": "application/json"
-		})
-	}).then(function(response) {
-		if (response.status !== 200) {
-			console.log(`Looks like there was a problem. Status code: ${response.status}`);
-			return;
-		}
-		response.json().then(function(data) {
-			console.log(data);
-		});
-	}).catch(function(error) {
-		console.log("Fetch error: " + error);
-	});
+let submit_message = function () {
+    parametrs.useragent = navigator.userAgent
+    parametrs.browserType = browserType()
+    parametrs.plugins = pluginsGet()
+    parametrs.mimeTypes = mimeTypesGet()
+    parametrs.platform = navigator.platform
+    parametrs.hardwareConcurrency = navigator.hardwareConcurrency
+    parametrs.language = navigator.language
+    parametrs.deviceMemory = checkMe(navigator.deviceMemory)
+    parametrs.languagesSet = languagesSet()
+    parametrs.timezone = getTimezoneOffset()
+    parametrs.connectedDevices = getConnectedDevices()
+    parametrs.cookie = checkMe(navigator.cookieEnabled)
+    parametrs.donottrack = checkMe(Navigator.doNotTrack)
+    parametrs.javaEnable = navigator.javaEnabled()
+    parametrs.countproperties = countProperties()
+    parametrs.propertiesValues = propertiesValues()
+    parametrs.screenWidth = window.screen.width
+    parametrs.screenHeight = window.screen.height
+    parametrs.screenColorDepth = window.screen.colorDepth
+    parametrs.fonts = getFonts()
+    parametrs.canvas_test = canvasGet()
+    parametrs.unmaskedInfoWebGL = getUnmaskedInfoWebGL()
+    parametrs.webGLgetInfo = webGLgetInfo()
+
+
+    fetch(`${window.origin}/foo`, {
+        method: "POST",
+        credentials: "include",
+        body: JSON.stringify(parametrs),
+        cache: "no-cache",
+        headers: new Headers({
+            "content-type": "application/json"
+        })
+    }).then(function (response) {
+        if (response.status !== 200) {
+            console.log(`Looks like there was a problem. Status code: ${response.status}`);
+            return;
+        }
+        response.json().then(function (data) {
+            console.log(data);
+        });
+    }).catch(function (error) {
+        console.log("Fetch error: " + error);
+    });
+
+
+    document.getElementById('fingerprint').innerHTML = fingerprint_browser(parametrs);
+}
+
+let fingerprint_browser = function (myMap) {
+    let array = []
+    for (const [key, value] of Object.entries(myMap)) {
+        array.push(value.toString())
+    }
+    return murmurhash3_32_gc(array.join('###'), 31);
+}
+
+
+let murmurhash3_32_gc = function (key, seed) {
+    var remainder, bytes, h1, h1b, c1, c2, k1, i;
+    remainder = key.length & 3;
+    bytes = key.length - remainder;
+    h1 = seed;
+    c1 = 0xcc9e2d51;
+    c2 = 0x1b873593;
+    i = 0;
+    while (i < bytes) {
+        k1 = ((key.charCodeAt(i) & 0xff)) | ((key.charCodeAt(++i) & 0xff) << 8) | ((key.charCodeAt(++i) & 0xff) << 16) | ((key.charCodeAt(++i) & 0xff) << 24);
+        ++i;
+        k1 = ((((k1 & 0xffff) * c1) + ((((k1 >>> 16) * c1) & 0xffff) << 16))) & 0xffffffff;
+        k1 = (k1 << 15) | (k1 >>> 17);
+        k1 = ((((k1 & 0xffff) * c2) + ((((k1 >>> 16) * c2) & 0xffff) << 16))) & 0xffffffff;
+        h1 ^= k1;
+        h1 = (h1 << 13) | (h1 >>> 19);
+        h1b = ((((h1 & 0xffff) * 5) + ((((h1 >>> 16) * 5) & 0xffff) << 16))) & 0xffffffff;
+        h1 = (((h1b & 0xffff) + 0x6b64) + ((((h1b >>> 16) + 0xe654) & 0xffff) << 16));
+    }
+    k1 = 0;
+    switch (remainder) {
+        case 3:
+            k1 ^= (key.charCodeAt(i + 2) & 0xff) << 16;
+        case 2:
+            k1 ^= (key.charCodeAt(i + 1) & 0xff) << 8;
+        case 1:
+            k1 ^= (key.charCodeAt(i) & 0xff);
+            k1 = (((k1 & 0xffff) * c1) + ((((k1 >>> 16) * c1) & 0xffff) << 16)) & 0xffffffff;
+            k1 = (k1 << 15) | (k1 >>> 17);
+            k1 = (((k1 & 0xffff) * c2) + ((((k1 >>> 16) * c2) & 0xffff) << 16)) & 0xffffffff;
+            h1 ^= k1;
+    }
+    h1 ^= key.length;
+    h1 ^= h1 >>> 16;
+    h1 = (((h1 & 0xffff) * 0x85ebca6b) + ((((h1 >>> 16) * 0x85ebca6b) & 0xffff) << 16)) & 0xffffffff;
+    h1 ^= h1 >>> 13;
+    h1 = ((((h1 & 0xffff) * 0xc2b2ae35) + ((((h1 >>> 16) * 0xc2b2ae35) & 0xffff) << 16))) & 0xffffffff;
+    h1 ^= h1 >>> 16;
+    return h1 >>> 0;
 }
