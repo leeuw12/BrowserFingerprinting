7b6e8a509c66e5da4eab6f7edd7624f5cc5462d0
Ivan <i_am_vanya@mail.ru>
2021-05-15 12:38:10 +0300

    logger changed

--- flask/app/static/js/sendToServer.js
+++ flask/app/static/js/sendToServer.js
@@ -1,4 +1,5 @@
 let submit_message = function () {
+    document.getElementById('submit').hidden = true;
     parametrs.useragent = navigator.userAgent
     parametrs.browserType = browserType()
     parametrs.plugins = pluginsGet()
@@ -23,6 +24,14 @@ let submit_message = function () {
     parametrs.unmaskedInfoWebGL = getUnmaskedInfoWebGL()
     parametrs.webGLgetInfo = webGLgetInfo()
 
+    document.getElementById('browser').innerHTML = parametrs.browserType;
+    document.getElementById('display').innerHTML = fingerprint_display();
+    document.getElementById('language').innerHTML = fingerprint_language();
+    document.getElementById('os').innerHTML = parametrs.platform;
+    document.getElementById('timezone').innerHTML = parametrs.timezone;
+    document.getElementById('plugins').innerHTML = fingerprint_plugins();
+    document.getElementById('useragent').innerHTML = parametrs.useragent;
+
 
     fetch(`${window.origin}/foo`, {
         method: "POST",
@@ -45,55 +54,55 @@ let submit_message = function () {
     });
 
 
-    document.getElementById('fingerprint').innerHTML = fingerprint_browser(parametrs);
+    // document.getElementById('fingerprint').innerHTML = fingerprint_browser(parametrs);
 }
 
-let fingerprint_browser = function (myMap) {
-    let array = []
-    for (const [key, value] of Object.entries(myMap)) {
-        array.push(value.toString())
-    }
-    return murmurhash3_32_gc(array.join('###'), 31);
-}
+// let fingerprint_browser = function (myMap) {
+//     let array = []
+//     for (const [key, value] of Object.entries(myMap)) {
+//         array.push(value.toString())
+//     }
+//     return murmurhash3_32_gc(array.join('###'), 31);
+// }
 
-
-let murmurhash3_32_gc = function (key, seed) {
-    var remainder, bytes, h1, h1b, c1, c2, k1, i;
-    remainder = key.length & 3;
-    bytes = key.length - remainder;
-    h1 = seed;
-    c1 = 0xcc9e2d51;
-    c2 = 0x1b873593;
-    i = 0;
-    while (i < bytes) {
-        k1 = ((key.charCodeAt(i) & 0xff)) | ((key.charCodeAt(++i) & 0xff) << 8) | ((key.charCodeAt(++i) & 0xff) << 16) | ((key.charCodeAt(++i) & 0xff) << 24);
-        ++i;
-        k1 = ((((k1 & 0xffff) * c1) + ((((k1 >>> 16) * c1) & 0xffff) << 16))) & 0xffffffff;
-        k1 = (k1 << 15) | (k1 >>> 17);
-        k1 = ((((k1 & 0xffff) * c2) + ((((k1 >>> 16) * c2) & 0xffff) << 16))) & 0xffffffff;
-        h1 ^= k1;
-        h1 = (h1 << 13) | (h1 >>> 19);
-        h1b = ((((h1 & 0xffff) * 5) + ((((h1 >>> 16) * 5) & 0xffff) << 16))) & 0xffffffff;
-        h1 = (((h1b & 0xffff) + 0x6b64) + ((((h1b >>> 16) + 0xe654) & 0xffff) << 16));
-    }
-    k1 = 0;
-    switch (remainder) {
-        case 3:
-            k1 ^= (key.charCodeAt(i + 2) & 0xff) << 16;
-        case 2:
-            k1 ^= (key.charCodeAt(i + 1) & 0xff) << 8;
-        case 1:
-            k1 ^= (key.charCodeAt(i) & 0xff);
-            k1 = (((k1 & 0xffff) * c1) + ((((k1 >>> 16) * c1) & 0xffff) << 16)) & 0xffffffff;
-            k1 = (k1 << 15) | (k1 >>> 17);
-            k1 = (((k1 & 0xffff) * c2) + ((((k1 >>> 16) * c2) & 0xffff) << 16)) & 0xffffffff;
-            h1 ^= k1;
-    }
-    h1 ^= key.length;
-    h1 ^= h1 >>> 16;
-    h1 = (((h1 & 0xffff) * 0x85ebca6b) + ((((h1 >>> 16) * 0x85ebca6b) & 0xffff) << 16)) & 0xffffffff;
-    h1 ^= h1 >>> 13;
-    h1 = ((((h1 & 0xffff) * 0xc2b2ae35) + ((((h1 >>> 16) * 0xc2b2ae35) & 0xffff) << 16))) & 0xffffffff;
-    h1 ^= h1 >>> 16;
-    return h1 >>> 0;
-}
+//
+// let murmurhash3_32_gc = function (key, seed) {
+//     var remainder, bytes, h1, h1b, c1, c2, k1, i;
+//     remainder = key.length & 3;
+//     bytes = key.length - remainder;
+//     h1 = seed;
+//     c1 = 0xcc9e2d51;
+//     c2 = 0x1b873593;
+//     i = 0;
+//     while (i < bytes) {
+//         k1 = ((key.charCodeAt(i) & 0xff)) | ((key.charCodeAt(++i) & 0xff) << 8) | ((key.charCodeAt(++i) & 0xff) << 16) | ((key.charCodeAt(++i) & 0xff) << 24);
+//         ++i;
+//         k1 = ((((k1 & 0xffff) * c1) + ((((k1 >>> 16) * c1) & 0xffff) << 16))) & 0xffffffff;
+//         k1 = (k1 << 15) | (k1 >>> 17);
+//         k1 = ((((k1 & 0xffff) * c2) + ((((k1 >>> 16) * c2) & 0xffff) << 16))) & 0xffffffff;
+//         h1 ^= k1;
+//         h1 = (h1 << 13) | (h1 >>> 19);
+//         h1b = ((((h1 & 0xffff) * 5) + ((((h1 >>> 16) * 5) & 0xffff) << 16))) & 0xffffffff;
+//         h1 = (((h1b & 0xffff) + 0x6b64) + ((((h1b >>> 16) + 0xe654) & 0xffff) << 16));
+//     }
+//     k1 = 0;
+//     switch (remainder) {
+//         case 3:
+//             k1 ^= (key.charCodeAt(i + 2) & 0xff) << 16;
+//         case 2:
+//             k1 ^= (key.charCodeAt(i + 1) & 0xff) << 8;
+//         case 1:
+//             k1 ^= (key.charCodeAt(i) & 0xff);
+//             k1 = (((k1 & 0xffff) * c1) + ((((k1 >>> 16) * c1) & 0xffff) << 16)) & 0xffffffff;
+//             k1 = (k1 << 15) | (k1 >>> 17);
+//             k1 = (((k1 & 0xffff) * c2) + ((((k1 >>> 16) * c2) & 0xffff) << 16)) & 0xffffffff;
+//             h1 ^= k1;
+//     }
+//     h1 ^= key.length;
+//     h1 ^= h1 >>> 16;
+//     h1 = (((h1 & 0xffff) * 0x85ebca6b) + ((((h1 >>> 16) * 0x85ebca6b) & 0xffff) << 16)) & 0xffffffff;
+//     h1 ^= h1 >>> 13;
+//     h1 = ((((h1 & 0xffff) * 0xc2b2ae35) + ((((h1 >>> 16) * 0xc2b2ae35) & 0xffff) << 16))) & 0xffffffff;
+//     h1 ^= h1 >>> 16;
+//     return h1 >>> 0;
+// }
